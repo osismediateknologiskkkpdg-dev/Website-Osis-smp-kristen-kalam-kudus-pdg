363774307161a0c892e02a323a43a068dd49d425
osismediateknologiskkkpdg-dev <osismediateknologiskkkpdg@gmail.com>
2026-08-31 09:25:20 +0700

    Change OTP UI and Navbar Maybe???

---
+++ components/demos/navbar-1-demo.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import * as React from "react";
+import { Navbar1 } from "@/components/ui/navbar-1";
+
+export default function Demo() {
+  return (
+    <div className="min-h-screen bg-stone-100 dark:bg-stone-900">
+      <Navbar1 />
+    </div>
+  );
+}
+
+export { Demo };
